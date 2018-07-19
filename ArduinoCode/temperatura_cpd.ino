a1e9039f525153455ae4ec35d1b4c0dc7560a24c
lfiguerola <laurafipe@gmail.com>
2018-07-19 11:28:13 +0200

    files updated

---
+++ ArduinoCode/temperatura_cpd.ino
@@ -0,0 +1,107 @@
+// which analog pin to connect
+#define THERMISTORPIN A0         
+// resistance at 25 degrees C
+#define THERMISTORNOMINAL 10000      
+// temp. for nominal resistance (almost always 25 C)
+#define TEMPERATURENOMINAL 25   
+// how many samples to take and average, more takes longer
+// but is more 'smooth'
+#define NUMSAMPLES 5
+// The beta coefficient of the thermistor (usually 3000-4000)
+#define BCOEFFICIENT 3950
+// the value of the 'other' resistor
+#define SERIESRESISTOR 10000    
+
+// Only prints when asked ('\n')
+#define USE_QUERY
+
+int samples[NUMSAMPLES];
+
+String inputString = "";         // a string to hold incoming data
+boolean stringComplete = false;  // whether the string is complete
+ 
+void setup(void) {
+  Serial.begin(9600);
+  analogReference(EXTERNAL);
+#ifdef USE_QUERY
+  // reserve 200 bytes for the inputString:
+  inputString.reserve(200);
+#endif
+}
+
+/*
+  SerialEvent occurs whenever a new data comes in the
+  hardware serial RX.  This routine is run between each
+  time loop() runs, so using delay inside loop can delay
+  response.  Multiple bytes of data may be available.
+ */
+#ifdef USE_QUERY
+void serialEvent() {
+  while (Serial.available()) {
+    // get the new byte:
+    char inChar = (char)Serial.read();
+    // add it to the inputString:
+    inputString += inChar;
+    // if the incoming character is a newline, set a flag
+    // so the main loop can do something about it:
+    if (inChar == '\n') {
+      stringComplete = true;
+    }
+  }
+}
+
+#endif
+
+ 
+void loop(void) {
+  uint8_t i;
+  float average;
+ 
+  // take N samples in a row, with a slight delay
+  for (i=0; i< NUMSAMPLES; i++) {
+   samples[i] = analogRead(THERMISTORPIN);
+   delay(10);
+  }
+ 
+  // average all the samples out
+  average = 0;
+  for (i=0; i< NUMSAMPLES; i++) {
+     average += samples[i];
+  }
+  average /= NUMSAMPLES;
+ 
+  //Serial.print("Average analog reading "); 
+  //Serial.println(average);
+ 
+  // convert the value to resistance
+  average = 1023 / average - 1;
+  average = SERIESRESISTOR / average;
+  //Serial.print("Thermistor resistance "); 
+  //Serial.println(average);
+ 
+  float steinhart;
+  steinhart = average / THERMISTORNOMINAL;     // (R/Ro)
+  steinhart = log(steinhart);                  // ln(R/Ro)
+  steinhart /= BCOEFFICIENT;                   // 1/B * ln(R/Ro)
+  steinhart += 1.0 / (TEMPERATURENOMINAL + 273.15); // + (1/To)
+  steinhart = 1.0 / steinhart;                 // Invert
+  steinhart -= 273.15;                         // convert to C
+
+#ifndef USE_QUERY
+  Serial.println(steinhart);
+  delay( 1000 );
+#else
+   // print the string when a newline arrives:
+    if (stringComplete) {
+      //Serial.println(inputString);
+      
+      // clear the string:
+      inputString = "";
+      stringComplete = false;
+      
+      Serial.println(steinhart);
+
+    }
+#endif
+}
+
